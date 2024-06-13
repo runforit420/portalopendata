86c9774f948df98437ab83d44ced0adae7dd66e4
runforit420 <gagu.cristi@gmail.com>
2024-06-13 20:03:16 +0300

    deleted importmap.rb

--- config/importmap.rb
+++
@@ -1,4 +0,0 @@
-import "@hotwired/turbo-rails"
-pin "@hotwired/turbo-rails", to: "turbo.js"
-pin_all_from "app/javascript/controllers", under: "controllers"
-pin "leaflet", to: "leaflet/dist/leaflet.js", preload: true
