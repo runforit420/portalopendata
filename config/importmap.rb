717435b33c63e5faa8cf84cd400878bfb4ce4a11
runforit420 <gagu.cristi@gmail.com>
2024-06-13 19:59:41 +0300

    Fix SCSS syntax errors in application.scss

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,3 +1,4 @@
+import "@hotwired/turbo-rails"
 pin "@hotwired/turbo-rails", to: "turbo.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "leaflet", to: "leaflet/dist/leaflet.js", preload: true
